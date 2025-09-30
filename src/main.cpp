bec38d1f9ca1e446f4d82eb32f5af7c932ecbfa7
Felix Allard <xilef992@gmail.com>
2025-09-30 15:51:30 -0400

    Test motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <Arduino.h>
 #include <LibRobus.h>
-#include <CEntryPoint.h>
+
 void setup() {
-    SetupC();
+    BoardInit();
+    // GIVE A LITTLE TIME FOR BOARD TO INIT
+    delay(1000);
+    MOTOR_SetSpeed(1, 0.1);
+    MOTOR_SetSpeed(0, 0.1);
+    delay(5000);
+    MOTOR_SetSpeed(1, 0);
+    MOTOR_SetSpeed(0, 0);
+
 }
 
 void loop() {
-    LoopC();
+
 }
