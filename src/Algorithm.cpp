7b3a72c0bc6cd8c4b3392f69213adaa5d4592625
dani-jrmll <danielajas@icloud.com>
2025-10-02 10:18:21 -0400

    ajouté des trucs a checkiftape

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -8,8 +8,31 @@ CurrentTile current_tile;
 
 void CheckIfTape(){
     if ((positionY %2 != 0) && (positionX == 1)) {
-
         current_tile.leftWall=true;
+        current_tile.straightWall=false;
+        current_tile.rightWall=true;
+        current_tile.backWall=false;
+    }
+    if (positionY == 0) {
+        current_tile.leftWall=false;
+        current_tile.straightWall=false;
+        current_tile.rightWall=false;
+        current_tile.backWall=true;
+    }
+    if (positionY == 9) {
+        current_tile.leftWall=false;
+        current_tile.straightWall=true;
+        current_tile.rightWall=false;
+        current_tile.backWall=false;
+    }
+    if (positionX == 0) {
+        current_tile.leftWall=true;
+        current_tile.straightWall=false;
+        current_tile.rightWall=false;
+        current_tile.backWall=false;
+    }
+    if (positionX == 2) {
+        current_tile.leftWall=false;
         current_tile.straightWall=false;
         current_tile.rightWall=true;
         current_tile.backWall=false;
