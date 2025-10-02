4c6e558c2083e5c4629fcfbc8c4da8dc3fde22b3
dani-jrmll <danielajas@icloud.com>
2025-10-02 10:11:20 -0400

    ajouté checkiftape

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Algorithm.h"
+
+CurrentTile current_tile;
+
+void CheckIfTape(){
+    if ((positionY %2 != 0) && (positionX == 1)) {
+
+        current_tile.leftWall=true;
+        current_tile.straightWall=false;
+        current_tile.rightWall=true;
+        current_tile.backWall=false;
+    }
+}
