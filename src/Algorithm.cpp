059f7231bd1c1349fd51d5b785516fc25a8c4a27
dani-jrmll <danielajas@icloud.com>
2025-10-02 10:29:20 -0400

    ajouté ResetCurrentTile

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -3,38 +3,50 @@
 //
 
 #include "Algorithm.h"
-
 CurrentTile current_tile;
 
+void ResetCurrentTile() {
+    current_tile.leftWall=false;
+    current_tile.straightWall=false;
+    current_tile.rightWall=false;
+    current_tile.backWall=false;
+    CheckIfTape();
+}
+
+
 void CheckIfTape(){
-    if ((positionY %2 != 0) && (positionX == 1)) {
+    if ((positionX == 1) && (positionY %2 != 0)) {
         current_tile.leftWall=true;
         current_tile.straightWall=false;
         current_tile.rightWall=true;
-        current_tile.backWall=false;
-    }
-    if (positionY == 0) {
-        current_tile.leftWall=false;
-        current_tile.straightWall=false;
-        current_tile.rightWall=false;
-        current_tile.backWall=true;
-    }
-    if (positionY == 9) {
-        current_tile.leftWall=false;
-        current_tile.straightWall=true;
-        current_tile.rightWall=false;
-        current_tile.backWall=false;
-    }
+        current_tile.backWall=false;}
+
     if (positionX == 0) {
         current_tile.leftWall=true;
         current_tile.straightWall=false;
         current_tile.rightWall=false;
-        current_tile.backWall=false;
-    }
+        current_tile.backWall=false;}
+
     if (positionX == 2) {
         current_tile.leftWall=false;
         current_tile.straightWall=false;
         current_tile.rightWall=true;
-        current_tile.backWall=false;
-    }
+        current_tile.backWall=false;}
+
+    if (positionY == 0) {
+        current_tile.leftWall=false;
+        current_tile.straightWall=false;
+        current_tile.rightWall=false;
+        current_tile.backWall=true;}
+
+    if (positionY == 9) {
+        current_tile.leftWall=false;
+        current_tile.straightWall=true;
+        current_tile.rightWall=false;
+        current_tile.backWall=false;}
+}
+
+void GetNextMovement() {
+
+
 }
