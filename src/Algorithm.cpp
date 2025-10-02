566b6aabcdf1459d13ae4246557f494a9786725b
evalourdess <evalourdes@icloud.com>
2025-10-02 11:10:47 -0400

    modified getnextmovement

--- src/Algorithm.cpp
+++ src/Algorithm.cpp
@@ -71,12 +71,32 @@ bool TestFrontWall() {
 }
 
 
-int GetNextMovement() {
-    if (current_tile.straightWall==true;
+/**
+ * @brief Choisit le prochain mouvement à effectuer.
+ * Retourne :
+ *   0 = avancer
+ *   1 = tourner à gauche
+ *   2 = tourner à droite
+ *   3 = demi-tour
+ */
 
-    current_tile.straightWall=true;
-    current_tile.rightWall=false;
-    current_tile.backWall=false;)}
+int GetNextMovement() {
+    // priorité : avancer > gauche > droite > demi-tour
+    if (!current_tile.straightWall) {
+
+        return 0; // continuer d'avancer
+    }
+    else if (!current_tile.leftWall) {
+        return 1; // tourner vers la gauche
+    }
+    else if (!current_tile.rightWall) {
+        return 2; // tourner vers la droite
+    }
+    else {
+        return 3; // faire un demi-tour
+    }
+}
+
 
 
 }
